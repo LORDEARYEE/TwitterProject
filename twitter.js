6b4c82de6eb3ff2ffc5859f761bbc1184f9e3f49
David Oddoye <david@integratorsb2b.com>
2016-07-25 14:12:55 +0000

    Big fixes

--- twitter.js
+++ twitter.js
@@ -3,7 +3,7 @@ var Twitter = require('twitter');
 var twitter = new Twitter({
   consumer_key: '0e58AJPd1SxD99EUbKXAuxWMI',
   consumer_secret: 'jKilF3urjDiyGZh9YAWqqGx0u6Xo6HIEMWFXfo2muuVp6jPifa',
-  access_token_key: 'sPCmeO9nrDw3YE1kggxTAzbJb2WCwTM0zyfDkF2',
+  access_token_key: '1688039894-sPCmeO9nrDw3YE1kggxTAzbJb2WCwTM0zyfDkF2',
   access_token_secret: 'pieEMrun8XTb4rIWUhiPTSarsMKjJCsoIt1NE5MAQzue0'
 });
 
@@ -11,10 +11,10 @@ var twitter = new Twitter({
 var count = 0,
 	util = require('util');
 
-twitter.stream('filter',{track: 'love'}, function(stream) {
+twitter.stream('statuses/filter', {track: 'love'}, function(stream) {
 
-	stream.on('data', function() {
-		console.log(util.inspect(data));
+	stream.on('data', function(tweet) {
+		console.log(util.inspect(tweet));
 		stream.destroy();
 		process.exit(0);
 	});
